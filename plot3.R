824bc8a7cc5639834e849d9b9e03b460133df3bd
rpsandell <ryan.sandell@gmail.com>
2015-09-12 21:19:57 -0700

    Fixed plot 3
    Added legend to plot 3

--- plot3.R
+++ plot3.R
@@ -11,4 +11,5 @@ plot(1:2880, power.data$Sub_metering_1, type="l", ylab="Energy sub metering", xl
 lines(1:2880, power.data$Sub_metering_2, col="red")
 lines(1:2880, power.data$Sub_metering_3, col="blue")
 axis(side=1, at=c(0, 1441, 2880), labels=c("Thu", "Fri", "Sat")) # add label of days at x=0 (beginning of Thu), x=1441 (beginning of Fri), and x=2880 (end of Fri)
+legend("topright", pch="l", col=c("black", "red", "blue"), legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 dev.off()
